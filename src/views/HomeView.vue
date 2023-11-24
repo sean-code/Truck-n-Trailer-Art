00a0d5d483926ec3c931da94339c9069677493d6
sean-code <ngangajohn536@gmail.com>
2023-11-24 13:34:11 +0300

    Update: New Changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,98 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
+
+      <!-- <p>2nd Part</p> -->
+  
+      <v-row cols="12" md="4">
+      
+        <v-col cols>
+
+          <h2 style="color: black; text-align: center;">Vehicle</h2>
+          <div style="width: 180px; height: 450px; background-color: #003366; position: relative;">
+          <!-- Top Wheels -->
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -35px; top: 0px; cursor: pointer;"></div>
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 184px; top: 0px; cursor: pointer;"></div>
+
+  
+          <!-- Mid Wheels -->
+          <div  @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -35px; top: 280px; cursor: pointer;"></div>
+          <div @click=""  style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -72px; top: 280px; cursor: pointer;"></div>
+  
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 184px; top: 280px; cursor: pointer;"></div>
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 220px; top: 280px; cursor: pointer;"></div>
+          
+          <!-- Bottom Wheels -->
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -35px; top: 380px; cursor: pointer;"></div>
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -72px; top: 380px; cursor: pointer;"></div>
+          
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 184px; top: 380px; cursor: pointer;"></div>
+          <div @click="" style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 220px; top: 380px; cursor: pointer;"></div>
+  
+          <!-- Spare Wheel -->
+          <div style="width: 32px; height: 70px; background-color: #DDDDDD; border-radius: 5px; position: absolute; left: 75px; top: 320px;">
+          </div>
+      </div>
+      
+      <!-- Vehicle Attached Part -->
+      <div style="width: 180px; height: 20px; background-color: #054a05; position: relative; top: 12px; margin-bottom: 15px;">
+        <p class="text-center"></p>
+      </div>
+        </v-col>
+
+
+        <v-col>
+            <!-- Trailer -->
+            <h2 style="color: black; text-align: center;">Trailer</h2>
+            <!-- Trailer Design Structure -->
+          <div style="width: 180px; height: 450px; background-color: #003300; position: relative;">
+                <!-- First Set of Wheels -->
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -35px; top: 180px;"></div>
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -72px; top: 180px;"></div>
+        
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 184px; top: 180px;"></div>
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 220px; top: 180px;"></div>
+        
+                <!-- Second Set of Wheels -->
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -35px; top: 280px;"></div>
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -72px; top: 280px;"></div>
+        
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 184px; top: 280px;"></div>
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 220px; top: 280px;"></div>
+                
+                <!-- Third Set of Wheels -->
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -35px; top: 380px;"></div>
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: -72px; top: 380px;"></div>
+                
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 184px; top: 380px;"></div>
+                <div style="width: 32px; height: 70px; background-color: black; border-radius: 5px; position: absolute; left: 220px; top: 380px;"></div>
+        
+                <!-- Spare Wheel -->
+                <div style="width: 32px; height: 70px; background-color: #DDDDDD; border-radius: 5px; position: absolute; left: 75px; top: 320px;"></div>
+            </div>
+            
+            <!-- Trailer Attached Part -->
+            <div style="width: 180px; height: 20px; background-color: #054a05; position: relative; top: 12px;">
+              <p class="text-center"></p>
+            </div>
+
+
+          </v-col>
+
+
+
+
+      </v-row>
+  
+
+
+  
+  
+  
+  
+  
+
+  </template>
